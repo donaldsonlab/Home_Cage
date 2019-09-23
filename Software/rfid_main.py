5b8e8b8e024b25c68722e1557fb81c81ca173364
Ryan <ryca1116@colorado.edu>
2019-09-23 12:00:48 -0600

    Inital Debug

--- Software/rfid_main.py
+++ Software/rfid_main.py
@@ -20,10 +20,12 @@ vole_2 = "736C8E"
 
 voleTags = queue.LifoQueue() #Initialize a LIFO (last-in first-out) queue to share with the other process?. Might not be the way to do this
 timeQueue = queue.LifoQueue() #Not sure about this one, we'll see how I want to structure this variable
-    timeQueue.put(0) #Initialize the Queue with a value of zero
+timeQueue.put(0) #Initialize the Queue with a value of zero
 numThreads = 2 #Number of threads to be running
 
 def rfidTrack_1():
+    print("begin1")
+    threadNum = 0;
     #Defines the serial port and the binary data stream characteristics
     serial_1 = serial.Serial(
         port = '/dev/serial0',
@@ -34,27 +36,37 @@ def rfidTrack_1():
     )
 
     while True:
+        print("istrue1")
         line_1 = serial_1.readline()
         if vole_1 in line_1.decode(): 
             voleTage.put("vole_1,1")
         if vole_2 in line_1.decode():
             voleTags.put("vole_2,1")
         
-        if KeyboardInterrupt:
-            atexit.register(end)
-            print("RFID 1")
-            print("Queue")
-            print(list(voleTags.queue))
-            print("")
+#        if KeyboardInterrupt:
+#            atexit.register(end)
+#            print("RFID 1")
+#            print("Queue")
+#            print(list(voleTags.queue))
+#            print("")
         #This block of code waits until all threads are finished running to move on
+        print("beginCheck1")
         threadNum = timeQueue.get()
         threadNum = threadNum + 1
         timeQueue.put(threadNum)
+        timeQueue.task_done()
+        print(threadNum)
+        print("Initial_1")
         while threadNum < numThreads:
             threadNum = timeQueue.get()
+            print(threadNum)
+            print("rfid_1")
+            timeQueue.task_done()
 
     #Include some wait condition for the other threads to end as well
 def rfidTrack_2():
+    print("begin2")
+    threadNum = 0
     serial_2 = serial.Serial(
         port = '/dev/ttySC0',
         baudrate = 9600,
@@ -64,24 +76,35 @@ def rfidTrack_2():
     )
 
     while True:
+        print("istrue2")
         line_2 = serial_2.readline()
         if vole_1 in line_2.decode(): 
             voleTags.put("vole_1,1")
         if vole_2 in line_2.decode():
             voleTags.put("vole_2,1")
         
-        if KeyboardInterrupt:
-            atexit.register(end)
-            print("RFID 1")
-            print("Queue")
-            print(list(voleTags.queue))
-            print("")
+#        if KeyboardInterrupt:
+#            atexit.register(end)
+#            print("RFID 1")
+#            print("Queue")
+#            print(list(voleTags.queue))
+#            print("")
         #This block of code waits until all threads are finished running to move on
+        print("beginCheck2")
+        print(timeQueue.get())
         threadNum = timeQueue.get()
+        print("midcheck2")
         threadNum = threadNum + 1
+        print("midcheck2")
         timeQueue.put(threadNum)
+        print(threadNum)
+        print("Initial_2")
+        timeQueue.task_done()
         while threadNum < numThreads:
             threadNum = timeQueue.get()
+            print(threadNum)
+            print("rfid_2")
+            timeQueue.task_done()
 
 def end():
     serial1.join()
@@ -95,7 +118,7 @@ def end():
 if __name__ == '__main__':
     #Creates the threads that track RFID movements
     serial1 = threading.Thread(target = rfidTrack_1)
-    serial2 = threading.Thread(target = rfidTracl_2)
+    serial2 = threading.Thread(target = rfidTrack_2)
 
     serial1.start()
     serial2.start()
