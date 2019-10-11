a770d2d9418a868166e6a80f10cde59bf7a713f2
ryca1116 <ryca1116@colorado.edu>
2019-10-11 11:51:27 -0600

    Threaded doors_main

--- Software/Modal/doors_main.py
+++ Software/Modal/doors_main.py
@@ -14,75 +14,100 @@ from Software.Modal import doors
 from Software.Modal import rfidLib as rfid
 from Software.RFID.rfid_main import vole
 import time
+import threading
+import queue
 
-#####################################################################################
-#Setup
-#####################################################################################
-leverPin1 = 17
-leverPin2 = 18
+def mode1(initialPos):
+    #####################################################################################
+    #MODE 1
+    #####################################################################################
+    
+    #Depending on the intitial position of the vole, wait for the lever. 
+    if initialPos == 1:
+        GPIO.wait_for_edge(leverPin1, GPIO_RISING)
+    elif initialPos == 2:
+        GPIO.wait_for_edge(leverPin2, GPIO_RISING)
 
-#This is the variable that is the servo controller
-kit = ServoKit(channels=16)
+    #Now do the door logic
+    doors.openDoor(kit, .7, 0)
+    thread_mode2.start()
 
-#Setup the pins for levers
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(leverPin1, GPIO.IN) #Sets up channel 17 as the lever channel
-GPIO.setup(leverPin2, GPIO.IN)
+def mode2():
+    ######################################################################################
+    #MODE 2
+    #####################################################################################
 
-#Now find which cage the animal is first in
-RFID_initialTag = rfid.get()
-if "vole_1" in RFID_initialTag[0]:
-    initialPos = int(RFID_initialTag[1]) #Initial cage number of the vole
-else:
-    RFID_initialTag = rfid.findTag("vole_1")
-    initialPos = int(RFID_initialTag[1])
+    #Loop that waits for RFID tag to pass.
+    #IF passed     -> move to MODE 3
+    #IF not passed -> wait for animal to pass, update RFID pings
+    #    IF time passed -> close door, move back to MODE 1
 
-#Optional Manual initial Position
-#initialPos = 1
+    timeout = 15 #Amount of time for the door to remain open
+    startTime = time.time() #Gets the time in seconds
+    while True:
+        newTime = time.time()
+        elapsedTime = newTime - startTime
+        if elapsedTime > timeout:
+            thread_mode1.start()
+            break #Move back to mode 1
 
-#####################################################################################
-#MODE 1
-#####################################################################################
+        #Find most recent positions of the animals
+        vole1 = rfid.findPos(1) #Test animal
+        vole2 = rfid.findPos(2) #Partner Animal
 
-#Depending on the intitial position of the vole, wait for the lever. 
-if initialPos == 1:
-    GPIO.wait_for_edge(leverPin1, GPIO_RISING)
-elif initialPos == 2:
-    GPIO.wait_for_edge(leverPin2, GPIO_RISING)
+        #REMEMBER - at the beginning, the animals are in separate cages
+        if vole1.pos == vole2.pos:
+            thread_mode3.start()
+            break #move on to mode 3
 
-#Now do the door logic
-doors.openDoor(kit, .7, 0)
+def mode3():
+    #####################################################################################
+    #MODE 3
+    #####################################################################################
 
-#Wait for RFID Tag that we passed
-######################################################################################
-#MODE 2
-#####################################################################################
+    #Just continuously update RFID marks
+    #IF animal in same cage -> continue update
+    #IF animals separate -> move back to MODE 2
+    #Track the position variable in the vole class
 
-#Loop that waits for RFID tag to pass.
-#IF passed     -> move to MODE 3
-#IF not passed -> wait for animal to pass, update RFID pings
-#    IF time passed -> close door, move back to MODE 1
+    while True:
+        vole1 = rfid.getVole(1)
+        if vole1.transition == 1:
+            thread_mode2.start()
+            break #go to mode 2
+        #Also track animal 2 if necessary, don't know if it is though
 
-timeout = 15 #Amount of time for the door to remain open
-startTime = time.time() #Gets the time in seconds
-while True:
-    newTime = time.time()
-    elapsedTime = newTime - startTime
-    if elapsedTime > timeout:
-        break
+if __name__ == "__main__":
+    #####################################################################################
+    #Setup
+    #####################################################################################
+    leverPin1 = 17
+    leverPin2 = 18
 
-    #Find most recent positions of the animals
-    vole1 = rfid.findPos(1) #Test animal
-    vole2 = rfid.findPos(2) #Partner Animal
+    #This is the variable that is the servo controller
+    kit = ServoKit(channels=16)
 
-    #REMEMBER - at the beginning, the animals are in separate cages
-    if vole1.pos == vole2.pos:
-        break #move on to mode 3
+    #Setup the pins for levers
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(leverPin1, GPIO.IN) #Sets up channel 17 as the lever channel
+    GPIO.setup(leverPin2, GPIO.IN)
 
-#####################################################################################
-#MODE 3
-#####################################################################################
+    #Now find which cage the animal is first in
+    RFID_initialTag = rfid.get()
+    if "vole_1" in RFID_initialTag[0]:
+        initialPos = int(RFID_initialTag[1]) #Initial cage number of the vole
+    else:
+        RFID_initialTag = rfid.findTag("vole_1")
+        initialPos = int(RFID_initialTag[1])
 
-#Just continuously update RFID marks
-#IF animal in same cage -> continue update
-#IF animals separate -> move back to MODE 2
+    #Optional Manual initial Position
+    #initialPos = 1
+
+    #####################################################################################
+    #Start the threading
+    #####################################################################################
+    thread_mode1 = threading.Thread(target=mode1) #Start mode 1 thread
+    thread_mode2 = threading.Thread(target=mode2)
+    thread_mode3 = threading.Thread(target=mode3)
+
+    thread_mode1.start()
