90ad0a4dfe3c53655725e6b29b092f3c4727fbf3
Ryan <ryca1116@colorado.edu>
2019-11-15 13:11:21 -0700

    Issue in the doors script found

--- Software/RFID/rfid_main.py
+++ Software/RFID/rfid_main.py
@@ -125,7 +125,7 @@ def rfidTrack_2(eventDict,voleDict):
 
     while True:
         event2.clear()
-        print("start2\n")
+        #print("start2\n")
         line_2 = serial_2.readline()
         if vole_1 in line_2.decode(): 
             print('Vole 1 Ping 2')
@@ -172,7 +172,7 @@ def rfidTrack_2(eventDict,voleDict):
         #print("beginCheck2\n")
         event2.set()
         mainEvent.wait()
-        print("complete2\n")
+        #print("complete2\n")
 
 def threadTrack(eventDict):
     mainEvent = eventDict.get("mainEvent")
