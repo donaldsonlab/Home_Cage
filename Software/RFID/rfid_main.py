724fc8e04fade89a2a25d7e133892b1b694362ac
Ryan-Desktop <lc.cameroon@gmail.com>
2021-01-23 14:17:44 -0700

    Finished debug
    Commented out the print statements for debug. Don't know what the issue was, after the board was unplugged from the pi and put back in it worked.

--- Software/RFID/rfid_main.py
+++ Software/RFID/rfid_main.py
@@ -96,8 +96,9 @@ def rfidTrack_2(eventDict,voleDict):
     while True:
         event2.clear()
         line_2 = serial_2.readline()
-        print("Line 2")
-        print(line_2)
+        # Print statements for debugging
+        #print("Line 2")
+        #print(line_2)
         if vole_1 in line_2.decode(): 
             print('Vole 1 Ping 2')
             if voleComm1.transition == 0: #Entering transition
