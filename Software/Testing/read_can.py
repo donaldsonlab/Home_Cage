f73a2b385594dc0253a4cc74dfc4da94be7f4603
Ryan-Desktop <lc.cameroon@gmail.com>
2022-01-05 14:16:04 -0700

    update

--- Software/Testing/read_can.py
+++ Software/Testing/read_can.py
@@ -1,6 +1,6 @@
 """ 
 Date Created : 9/2/2021
-Date Modified: 9/2/2021
+Date Modified: 11/18/2021
 Author: Ryan Cameron
 Description: is the file that contains all of the necessary classes and functions to read data coming in through a CAN-bus line on the raspberry pi and send/store it for reference.
 Property of Donaldson Lab, University of Colorado Boulder, PI Zoe Donaldson
@@ -14,25 +14,33 @@ import serial
 import time
 
 # Classes
+class message:
+    """MESSAGE class is what contains the message data, string conversion, timestamp, and all relevant info and functions to recieve, read, and log the message.
+    """
+    def __init__(self):
+        self.timestamp = None
+        self.command = None
+        self.bus = can.interface.Bus() # Should auto configure the bus, make sure that this is interface.Bus NOT can.Bus
+        pass
+
+    # This function recieves the message through the CAN bus shield
+    def recieve(self):
+        """This function recieves the message and parses up the object to properties in the native object here. 
+        """
+        recMessage = self.bus.recv()
+        self.byteData = recMessage.data
+        self.command = self.byteData.hex()
+        print("Recieved Command: " + self.command)
 
 # Test Script
 """
-This section is for running test scripts for the code. If you want to run this test you must run this script as the main script. It will not be run if called from another file. 
+This section is for running test scripts for the code. If you want to run this test you must run this script as the main script. It will not be run if called from another file. This test specifically is meant to test the functionality of reading CAN bus data that is being sent to it over the pi hat.
 """
 
 if __name__ == "__main__": # If this is the main script
-    # Setup commands to send
-    voleTag = 1256
-    timestamp = time.time()
-    rfidNum = 4
-    data = bytearray([voleTag, rfidNum], encoding='hex')
+    # Setup the BUS to recieve CAN message from arduino
+    testMess = message()
 
-    # Setup the Bus
-    bus = can.Bus(channel='can0') # /dev port on the pi where the can bus is connected to I think
-
-    # Send the command to the arduino
-    msg = can.Message(timestamp=timestamp,data=data)
-    bus.send(msg=msg)
-
-    # Recieve confirmation from the Arduino
+    # Recieve the data
+    testMess.recieve()
 
--- Software/can_arduino/can_arduino.ino
+++ Software/can_arduino/can_arduino.ino
@@ -8,6 +8,8 @@
 // Variables
 int buttonPin = 4;
 int buttonState;
+tCAN message;
+
 // Setup
 void setup() {
   // Setup serial
@@ -16,13 +18,55 @@ void setup() {
 
   // Setup pins
   pinMode(buttonPin, INPUT);
-  
 }
 
 // Body
 void loop() {
   // Right now just test the button functionality
   buttonState = digitalRead(buttonPin);
-  Serial.println(buttonState);
-  delay(100);
+  if (buttonState == HIGH) {
+    // Create and send the data to the pi FOR NOW JUST PRINT IT TO THE SCREEN
+    Serial.println("Button Pushed");
+    tCAN messageData = create_data();
+    send_data(messageData); // Send it
+  }
+}
+
+// Send Data Script
+void send_data(tCAN message) {
+  // Format and send the message
+  // INPUTS: message (string) - the data string that will be sent to the raspberry pi
+  Serial.println("Sending...");
+  mcp2515_bit_modify(CANCTRL, (1<<REQOP2)|(1<<REQOP1)|(1<<REQOP0), 0);
+  mcp2515_send_message(&message);
+
+  // Find a way to print it???
+  Serial.println("Sent"); // Most likely this should be in the form of a software serial
+}
+
+// Create Data Function
+tCAN create_data() {
+  // Initially this function starts out with no input, but eventually will need to input the data and info that the arduino will send in the form of a can bus message.
+  // INPUTS: voleId (string) - ID tag of the vole
+  //         rfidId (string) - Rfid reader number
+  Serial.println("Creating...");
+
+  tCAN message;
+  message.id = 0x631; // HEX formatted message ID
+  message.header.rtr = 0;
+  message.header.length = 8; // Message length
+
+  // Vole Tag
+  message.data[0] = 0x6d;
+	message.data[1] = 0x32;
+	message.data[2] = 0x31;
+	message.data[3] = 0x35; //formatted in HEX
+	message.data[4] = 0x34;
+	message.data[5] = 0x66;
+
+  // RFID ID
+	message.data[6] = 0x30;
+	message.data[7] = 0x33; 
+  Serial.println("Message Created");
+  return message;
 }
