f7d1416b89937cb623559ba56b43f9ee25c1c670
dprotter <david.protter@gmail.com>
2022-01-06 13:36:02 -0700

    changes on the pi

--- Software/Testing/read_can.py
+++ Software/Testing/read_can.py
@@ -12,29 +12,39 @@ Code located at - https://github.com/donaldsonlab/Home_Cage
 import can 
 import serial
 import time
+from can.interfaces.serial.serial_can import *
 
 # Classes
 class message:
     """MESSAGE class is what contains the message data, string conversion, timestamp, and all relevant info and functions to recieve, read, and log the message.
     """
     def __init__(self, isserial = False):
+        print("Initializing Message...")
         self.timestamp = None
         self.command = None
 
         # Check if its a serial bus
         if isserial:
-            self.bus = can.interfaces.serial.serial_can.SerialBus(channel = "/dev/tty0")
+            print("Seting up serial bus...")
+            self.bus = SerialBus(channel = "/dev/tty1")
+            print("Serial bus created")
         else:
+            print("Setting up CAN bus...")
             self.bus = can.interface.Bus() # Should auto configure the bus, make sure that this is interface.Bus NOT can.Bus
-
-        pass
+            print("Bus created")
+        
+        print("Message object created")
 
     # This function recieves the message through the CAN bus shield
     def recieve(self):
         """This function recieves the message and parses up the object to properties in the native object here. 
         """
-        recMessage = self.bus.recv()
+
+        print("Attempting to recieve...")
+        recMessage = self.bus.recv(timeout = 3)
         print("Message Recieved")
+        print(recMessage)
+
         self.byteData = recMessage.data
         self.command = self.byteData.hex()
         print("Recieved Command: " + self.command)
@@ -51,7 +61,12 @@ class message:
             raise ValueError("Must be 8 bytes or shorter")
         
         # Send the message
+        print("Sending...")
         self.bus.send(msg = can.Message(data = data), timeout = 3)
+        print("Data sent")
+
+    def listen(self):
+        
 
 # Test Script
 """
